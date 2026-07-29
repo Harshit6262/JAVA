aa0f88c1c4c6be00a4f5d89a4c72149067de33e9
Harshit Jaiswal <harshitjaiswal0705@gmail.com>
2026-07-30 00:59:40 +0530

    String Linear Search

---
+++ Array/StringLS.java
@@ -0,0 +1,21 @@
+public class StringLS {
+    public static int linearSearch(String [] food, String key){
+        for(int i = 0; i<food.length; i++){
+            if(food[i] == key){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String [] args){
+        String food [] = {"Dosa", "Idli", "Momo", "Biryani"};
+        String key = "Biryani";
+        int dish = linearSearch(food, key);
+        if(dish == -1){
+            System.out.println("Not Available :)");
+        } else {
+            System.out.println("Your Dish is Available." );
+        }
+    } 
+    
+}
